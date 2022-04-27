4e5a7626cb4dea65ffc90f3f4c8540a324adecb7
matheus <matheus.rj25@hotmail.com>
2022-04-27 12:42:00 -0300

    add payment in website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
 import { AppProps } from 'next/app';
 import "../styles/global.scss";
 import { Header } from "../components/Header";
-import {Provider as NextAuthProvider} from "next-auth/client";
+import { Provider as NextAuthProvider } from "next-auth/client";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js"
+
+const initialOptions = {
+  "client-id":"AVixcvJrWHU5rsimyCj6GSHJhmNsRyT_T58bsJejVhY_aXmUuJ0A1o6Rvi5t0C36Pi9xVwbMu6e1WhrC",
+  currency: "BRL",
+  intent:"capture"
+}
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthProvider session={pageProps.session}>
-      <Header />
-      <Component {...pageProps} />
+      <PayPalScriptProvider options={initialOptions}>
+        <Header />
+        <Component {...pageProps} />
+      </PayPalScriptProvider>
     </NextAuthProvider>
   )
 }
---
+++ src/pages/donate/index.tsx
@@ -0,0 +1,106 @@
+import { GetServerSideProps } from 'next';
+import { useState } from 'react'; 
+import styles from './styles.module.scss';
+import Head from 'next/head'; 
+import { getSession } from 'next-auth/client';
+import firebase from '../../services/firebaseConnection';
+
+import { PayPalButtons } from '@paypal/react-paypal-js';
+
+
+interface DonatePorps{
+  user:{
+    name: string;
+    id: string;
+    image: string; 
+  }
+}
+
+export default function Donate({ user }: DonatePorps ){
+  const [vip, setVip] = useState(false);
+
+  async function handleSaveDonate(){
+    await firebase.firestore().collection('users')
+    .doc(user.id)
+    .set({
+      donate: true,
+      lastDonate: new Date(),
+      image: user.image
+    })
+    .then(()=>{
+      setVip(true);
+    })
+
+  }
+
+
+  return(
+    <>
+    <Head>
+      <title>Help the platform stay online!</title>
+    </Head>
+    <main className={styles.container}>
+      <img src="/images/rocket.svg" alt="Seja Apoiador" />
+      
+      {vip && (
+        <div className={styles.vip}>
+          <img src={user.image} alt="Foto de perfil do usuario" />
+          <span>Congrats you are a new support !</span>
+        </div>
+      )}
+
+      <h1>Seja um apoiador deste projeto 🏆</h1>
+      <h3>Contribua com apenas <span>R$ 1,00</span></h3>
+      <strong>Apareça na nossa home, tenha funcionalidades exclusivas.</strong>
+
+      <PayPalButtons
+        createOrder={ (data, actions) => {
+          return actions.order.create({
+            purchase_units: [{
+              amount:{
+                value: '1'
+              }
+            }]
+          })
+        }}
+        onApprove={ (data, actions) => {
+          return actions.order.capture().then(function(details){
+            console.log('Compra aprovada: ' + details.payer.name.given_name);
+            handleSaveDonate();
+          })
+        }}
+
+      />
+      
+    </main>    
+    </>
+  )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req })
+
+  if(!session?.id){
+    return{
+      redirect:{
+        destination: '/',
+        permanent: false
+      }
+    }
+  } 
+
+
+  const user = {
+    name: session?.user.name,
+    id: session?.id,
+    image: session?.user.image
+  }
+
+  return{
+    props:{
+      user
+    }
+  }
+
+}
