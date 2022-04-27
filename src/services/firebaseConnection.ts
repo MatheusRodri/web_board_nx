4dc8fb08f1831560710f8969ccdf17fb9395d0ca
matheus <matheus.rj25@hotmail.com>
2022-04-27 08:23:15 -0300

    add firebase

---
+++ src/services/firebaseConnection.ts
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+let firebaseConfig = {
+    apiKey: "AIzaSyDNSVOhXTDJI4QYjTmTJ60j0GtLExRcn6Y",
+    authDomain: "board-next-1755d.firebaseapp.com",
+    projectId: "board-next-1755d",
+    storageBucket: "board-next-1755d.appspot.com",
+    messagingSenderId: "271483913",
+    appId: "1:271483913:web:e2a1d2751bb2e0bfe924d8",
+    measurementId: "G-K0CXNM70VL"
+};
+
+if (!firebase.app.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
